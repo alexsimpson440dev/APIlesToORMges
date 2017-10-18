2f4aabd45910ad34b8c9ebd051e3c09876080273
Alexander Simpson <alexsimpson440dev@gmail.com>
2017-10-17 20:55:05 -0500

    Developing basic parts of project to merge to desktop setup.

--- src/server.py
+++ src/server.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='.', template_folder='../templates')
+app = Flask(__name__, static_folder='../static', template_folder='../templates')
 
 @app.route('/')
 @app.route('/index.html')
+@app.route('/index.html', methods=['get'])
 def index():
     return render_template('index.html')
 
